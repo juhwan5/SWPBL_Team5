7e2a9ec8f9aef25ed1df0dc7081d007df8b714f2
juhwan5 <lavistrike770@gmail.com>
2021-05-18 21:07:39 +0900

    5.18 merge ready

--- PRECTI~3/app/src/main/java/com/example/precticeproject/functions/FindAltAz.java
+++ PRECTI~3/app/src/main/java/com/example/precticeproject/functions/FindAltAz.java
@@ -3,105 +3,126 @@ package com.example.precticeproject.functions;
 
 import android.app.Activity;
 
-import com.example.precticeproject.R;
+
 
 public class FindAltAz {
-    public static String[] findAltAz(Activity activity, String m, String c, String t){
-        int meteor;
-        String[] a;
-        LoadRawCSV loadRawCSV = new LoadRawCSV(activity);
-        meteor = findmeteor(m);
-        if (meteor != -1 ) {
-            String[][] rawdata = loadRawCSV.loadDB(meteor);
+    public static String[] findAltAz(String m, String c, String t){
+        String[] keys = new String[4];
+        keys[0] = findmeteor(m);
+        keys[1] = findcity(c);
+        keys[2] = findtime(t);
+        keys[3] = findtime(t);
 
-            int city = findcity(c);
-            int time = findtime(t);
-            if (city != -1 & time != -1) {
-                String alt, az;
-                alt = rawdata[city][(time * 2) - 1];
-                az = rawdata[city][(time * 2)];
-                a = new String[]{alt, az};
-            } else{
-                a = new String[]{"검출 불가", "검출 불가"};
-            }
-        } else {
-            a = new String[]{"검출 불가", "검출 불가"};
+        if (! keys[2].equals("fail")) {
+            keys[2] = keys[2] + "-Alt.";
+            keys[3] = keys[3] + "-Az.";
         }
-        return a;
+        return keys;
     }
 
-
-    //유성우 종류 확인 -> 참고할 raw파일 확인
-    public static int findmeteor(String m){
-        int key;
+    //유성우 종류 확인
+    public static String findmeteor(String m){
+        String key;
         switch(m){
             case "거문고자리 유성우":
-                key = R.raw.lyrids;
+                key = "Lyrids";
                 break;
             case "사분의자리 유성우":
-                key = R.raw.quadrantids;
+                key = "Quadrantids";
                 break;
             case "페르세우스 유성우":
-                key = R.raw.perseids;
+                key = "Perseids";
                 break;
             case "사자자리 유성우":
-                key = R.raw.leonids;
+                key = "Leonids";
                 break;
             case "쌍둥이자리 유성우":
-                key = R.raw.geminids;
+                key = "Geminids";
+                break;
+            case "물병자리-에타 유성우":
+                key = "Eta Aquariids";
+                break;
+            case "물병자리-델타-남 유성우":
+                key = "S Delta Aquariids";
+                break;
+            case "오리온자리 유성우":
+                key = "Orionids";
                 break;
             default:
-                key = -1;
+                key = "fail";
         }
         return key;
     }
 
     //지역 확인
-    public static int findcity(String c){
-        int key;
+    public static String findcity(String c){
+        String key;
         switch(c){
-            case "서울":
-                key = 1;
+            case "강원":
+                key = "Gangwon";
+                break;
+            case "경기":
+                key = "Gyeonggi";
+                break;
+            case "경남":
+                key = "Gyeongsangnam";
+                break;
+            case "경북":
+                key = "Gyeongsangbuk";
+                break;
+                //4
+            case "광주":
+                key = "Gwangju";
+                break;
+            case "대구":
+                key = "Daegu";
+                break;
+            case "대전":
+                key = "Daejeon";
                 break;
             case "부산":
-                key = 2;
+                key = "Busan";
                 break;
-            case "제주":
-                key = 3;
+            //8
+            case "서울":
+                key = "Seoul";
                 break;
-            case "전북":
-                key = 4;
+            case "울산":
+                key = "Ulsan";
+                break;
+            case "인천":
+                key = "Incheon";
                 break;
             case "전남":
-                key = 5;
+                key = "Chollanam";
                 break;
+            //12
+            case "전북":
+                key = "Chollabuk";
+                break;
+            case "제주":
+                key = "Jeju";
+                break;
+            case "충남":
+                key = "Chungcheongnam";
+                break;
+            case "충북":
+                key = "Chungcheongbuk";
+                break;
+            //16
             default:
-                key = -1;
+                key = "fail";
         }
         return key;
     }
 
     //시각 확인
-    public static int findtime(String t){
-        int key;
-        switch(t){
-            case "00시":
-                key = 1;
-                break;
-            case "02시":
-                key = 2;
-                break;
-            case "04시":
-                key = 3;
-                break;
-            case "06시":
-                key = 4;
-                break;
-            case "08시":
-                key = 5;
-                break;
-            default:
-                key = -1;
+    public static String findtime(String t){
+        String key;
+        if (t.length() >= 3) {
+            key = t.substring(0, 2) + ":00";
+        }else {
+            return "fail";
         }
         return key;
     }
