8fec8b678be03f1165455892c334d69e17b54f39
juhwan5 <lavistrike770@gmail.com>
2021-05-25 17:26:43 +0900

    1.7.3
    - 홈에 웹 뷰 추가

--- PRECTI~3/app/src/main/java/com/example/precticeproject/fragments/HomeFragment.java
+++ PRECTI~3/app/src/main/java/com/example/precticeproject/fragments/HomeFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import androidx.fragment.app.Fragment;
 
@@ -11,6 +14,8 @@ import com.example.precticeproject.R;
 
 public class HomeFragment extends Fragment {
     public HomeFragment() {}
+    private WebView mWebView;
+    private WebSettings mWebSettings;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -19,7 +24,31 @@ public class HomeFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        return inflater.inflate(R.layout.home_frag,container,false);
+        View v = inflater.inflate(R.layout.home_frag,container,false);
+        mWebView = (WebView) v.findViewById(R.id.homewebView);
+
+        setWebView(mWebView,mWebSettings);
+
+        return v;
+    }
+
+    public void setWebView(WebView mWebView, WebSettings mWebSettings){
+
+        mWebView.setWebViewClient(new WebViewClient()); // 클릭시 새창 안뜨게
+        mWebSettings = mWebView.getSettings(); //세부 세팅 등록
+        mWebView.getSettings().setUseWideViewPort(true);
+        mWebView.getSettings().setLoadWithOverviewMode(true);
+        mWebSettings.setJavaScriptEnabled(true); // 웹페이지 자바스클비트 허용 여부
+        mWebSettings.setSupportMultipleWindows(false); // 새창 띄우기 허용 여부
+        mWebSettings.setJavaScriptCanOpenWindowsAutomatically(false); // 자바스크립트 새창 띄우기(멀티뷰) 허용 여부
+        mWebSettings.setLoadWithOverviewMode(true); // 메타태그 허용 여부
+        mWebSettings.setUseWideViewPort(true); // 화면 사이즈 맞추기 허용 여부
+        mWebSettings.setSupportZoom(false); // 화면 줌 허용 여부
+        mWebSettings.setBuiltInZoomControls(false); // 화면 확대 축소 허용 여부
+        mWebSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NORMAL); // 컨텐츠 사이즈 맞추기
+        mWebSettings.setCacheMode(WebSettings.LOAD_NO_CACHE); // 브라우저 캐시 허용 여부
+        mWebSettings.setDomStorageEnabled(true); // 로컬저장소 허용 여부
+        mWebView.loadUrl("http://shh906.dothome.co.kr/index.html"); // 웹뷰에 표시할 웹사이트 주소, 웹뷰 시작
     }
 
 }
